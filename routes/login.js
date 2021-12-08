36838d259a53352af657c1650b1668955e7feb73
ngotruongtuyen <ngotuyen781@gmail.com>
2021-12-08 21:28:39 +0700

    codes: update login

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('account/login');
+    res.render('auth/views/login');
 });
 
 module.exports = router;
