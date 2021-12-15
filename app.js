e7284cee36f338c9334ef128f0b76ebc5d9761fd
ngotruongtuyen <ngotuyen781@gmail.com>
2021-12-15 20:53:04 +0700

    codes: update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const session = require('express-session')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
 const registerRouter = require('./routes/register');
 const loginRouter = require('./routes/login');
@@ -21,7 +21,7 @@ const ApiRouter=require('./api/index')
 
 const passport = require("./passport");
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', [path.join(__dirname, 'views'), path.join(__dirname, 'components')]);
@@ -34,7 +34,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 console.log(process.env.SESSION_SECRET);
 app.use(session({ secret: process.env.SESSION_SECRET }));
@@ -45,6 +44,7 @@ app.use(function (req, res, next) {
   res.locals.user = req.user;
   next();
 })
+
 app.use('/', indexRouter);
 app.use('/', authRouter);
 app.use('/register', registerRouter);
@@ -56,7 +56,6 @@ app.use('/product-detail', productDetailsRouter);
 app.use('/contact', contactRouter);
 app.use('/cart',loggedInUserGuard,cartRouter)
 app.use('/api',ApiRouter)
-
 
 
 // catch 404 and forward to error handler
