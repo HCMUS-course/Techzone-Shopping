d02073757d6eb6cf72697a8c1e87aaf631d3b2e3
ngotruongtuyen <ngotuyen781@gmail.com>
2021-12-15 21:26:56 +0700

    codes: update app.js

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
 console.log(process.env.SESSION_SECRET);
 app.use(session({ secret: process.env.SESSION_SECRET }));
