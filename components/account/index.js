a42f0e14cec8e60a61b64edf6e8ca2526464089d
ngotruongtuyen <ngotuyen781@gmail.com>
2021-12-20 13:45:57 +0700

    codes: update edit profile url

--- components/account/index.js
+++ components/account/index.js
@@ -5,8 +5,8 @@ const accountController = require('./accountController');
 router.use('/profile/:route', express.static('public'));
 router.get('/profile/:id', accountController.getProfile);
 
-router.use('/edit', express.static('public'));
-router.get('/edit/:id', accountController.getEditProfile);
-router.post('/edit/:id', accountController.updateProfile);
+router.use('/profile/edit', express.static('public'));
+router.get('/profile/edit/:id', accountController.getEditProfile);
+router.post('/profile/edit/:id', accountController.updateProfile);
 
 module.exports = router;
