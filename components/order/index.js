5af19b050b91276df0106c7bf8403214cd29458a
19127324 <64062447+19127324@users.noreply.github.com>
2021-12-23 22:19:04 +0700

    update order 3

--- components/order/index.js
+++ components/order/index.js
@@ -1,61 +1,12 @@
 const express=require("express")
 const router=express.Router();
-const Cart = require("../cart/cartModel")
-const Order=require("./orderModel");
+
 const orderController=require("./orderController")
 
 router.use(express.static("public")); 
 router.get('/history',orderController.showHistory)
 router.get('/',orderController.list)
-
-router.post('/', async function(req, res){
-    
-    const userId = req.user._id;
-    
-    const cart =await Cart.findOne({userId:userId}).lean();
-   
-    const order =await Order.findOne({userId:userId}).lean();
-
-    const temp = {
-      cart : cart,
-      address : req.body.address,
-      phoneNumber : req.body.phone,
-      date : req.body.date,
-      status : "preparing"
-    }
-
-    const newvalues = { $set: {items: [], totalQty: 0,totalPrice: 0, userId :userId } };
-    await Cart.updateOne({userId:userId},newvalues)
-
-    if(order == null){
-      const newOrder = new Order();
-      
-      newOrder.items.push(temp);
-      newOrder.userId =  userId;
-    
-     
-      newOrder.save((err,doc)=>{
-        if(!err)
-        res.redirect('/cart');
-        
-        else{
-          console.log('Error during record inserted: '+ err);
-        }
-  
-      });
-    }
-    else{
-      
-      
-    
-      await Order.updateOne({userId : userId},{$push:{items:{$each:[temp],$position:0}}}) 
-      res.redirect('/cart');
-
-    }
-    
-    
-    
-  });
+router.post('/', orderController.createOrder)
 
 
 module.exports=router
--- components/order/orderController.js
+++ components/order/orderController.js
@@ -1,4 +1,6 @@
 const orderServices=require("./orderService")
+const Cart = require("../cart/cartModel")
+const Order=require("./orderModel");
 
 module.exports.list=async (req,res)=>{
     const userId=req.user._id
@@ -18,4 +20,46 @@ module.exports.showHistory=async (req,res)=>{
     //     temp = "hidden"
     // }
     res.render("order/views/history",{order:order})
+}
+
+module.exports.createOrder = async (req,res) => {
+    const userId = req.user._id;
+    
+    const cart =await Cart.findOne({userId:userId}).lean();
+   
+    const order =await Order.findOne({userId:userId}).lean();
+
+    const temp = {
+      cart : cart,
+      address : req.body.address,
+      phoneNumber : req.body.phone,
+      date : req.body.date,
+      status : "preparing"
+    }
+
+    const newvalues = { $set: {items: [], totalQty: 0,totalPrice: 0, userId :userId } };
+    await Cart.updateOne({userId:userId},newvalues)
+
+    if(order == null){
+      const newOrder = new Order();
+      
+      newOrder.items.push(temp);
+      newOrder.userId =  userId;
+    
+     
+      newOrder.save((err,doc)=>{
+        if(!err)
+        res.redirect('/order/history');
+        
+        else{
+          console.log('Error during record inserted: '+ err);
+        }
+  
+      });
+    }
+    else{ 
+      await Order.updateOne({userId : userId},{$push:{items:{$each:[temp],$position:0}}}) 
+      res.redirect('/order/history');
+
+    }
 }
