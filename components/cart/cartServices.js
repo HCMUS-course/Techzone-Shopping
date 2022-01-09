0e17a435390df561e04a581fbc0619f468c4208a
19127324 <64062447+19127324@users.noreply.github.com>
2022-01-10 00:09:25 +0700

    update add to cart

--- components/cart/cartServices.js
+++ components/cart/cartServices.js
@@ -1,5 +1,6 @@
 
 
+const Product = require('../products/productModel');
 const productService = require('../products/productService');
 const Cart=require("./cartModel")
 
@@ -10,6 +11,7 @@ return Cart.findOne({userId:userId}).lean()
 module.exports.postItem=async (userId,newItemId)=>{
    
     const newItem=await productService.detail(newItemId)
+    if(newItem != null){
     if(newItem.stock > 0){
         productService.updateStock(newItemId,-1)
     
@@ -24,11 +26,14 @@ module.exports.postItem=async (userId,newItemId)=>{
     return Cart.findOne({userId:userId}).lean()
 
     }
-
+}
 
     module.exports.deleteItem=async (userId,ItemId)=>{
    
         await productService.updateStock(ItemId,1)
+        const item = await productService.detail(ItemId)
+        const money = item.price
+        await Cart.updateOne({userId:userId},{$inc:{totalPrice : -money}})
         return Cart.updateOne({ userId: userId }, {
             $pull: { items: {_id:{$in: [ItemId]}} }
         },{ safe: true, upsert: true });
