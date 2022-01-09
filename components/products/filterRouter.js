d4ae6007a21c8dbdc5d8fe9c01e4f56ab7fa992e
NgoTruongTuyen <ngotuyen781@gmail.com>
2022-01-09 23:12:45 +0700

    Revert "codes: update filter"
    This reverts commit 7fae7f6e6bde3916f6f7ad8d42f0e0ce9bd235ff.

--- components/products/filterRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const productController = require('./productController');
-
-
-
-router.get('/filter', productController.filter);
-
-module.exports = router;
