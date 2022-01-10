aa349ebe14bd97518d649a96b73750771129b390
19127324 <64062447+19127324@users.noreply.github.com>
2022-01-10 19:21:13 +0700

    update history order view

--- components/order/index.js
+++ components/order/index.js
@@ -1,11 +1,12 @@
 const express=require("express")
 const router=express.Router();
-
 const orderController=require("./orderController")
 
-router.use(express.static("public")); 
-router.get('/history',orderController.showHistory)
+router.use('/',express.static("public")); 
 router.get('/',orderController.list)
+router.use('/history',express.static("public")); 
+router.get('/history',orderController.showHistory)
+
 router.post('/', orderController.createOrder)
 
 
